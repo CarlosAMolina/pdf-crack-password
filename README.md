38889e1763275dca1afe4a5194c3b5e502dc2fdc
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-29 20:15:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Introduction
+
+Project to automate the password craking of a PDF file.
+
+The PDF `secret-document.pdf` password is `AbC123`.
+
+## Run the program
+
+```python
+python crack-pdf.py
+```
+
+## Resources
+
+Source code: <https://www.instagram.com/p/CmTELtStKLr/>
---
+++ crack-pdf.py
@@ -0,0 +1,35 @@
+from tqdm import tqdm
+import pikepdf
+import typing as tp
+
+
+def run():
+    passwords = get_passwords()
+    password = get_pdf_password(passwords)
+    if password is None:
+        print(f"\n[-] No password found")
+    else:
+        print(f"\n[+] Password found: {password}")
+
+
+def get_passwords() -> tp.List[str]:
+    return [line.strip() for line in open("wordlist.txt")]
+
+
+def get_pdf_password(passwords: tp.List[str]) -> tp.Optional[str]:
+    for password in tqdm(passwords, "Decrypting PDF"):
+        print(f"Checking password: {password}")
+        if is_password_correct(password):
+            return password
+
+
+def is_password_correct(password: str) -> bool:
+    try:
+        with pikepdf.open("secret-document.pdf", password=password) as pdf:
+            return True
+    except pikepdf._qpdf.PasswordError as e:
+        return False
+
+
+if __name__ == "__main__":
+    run()
