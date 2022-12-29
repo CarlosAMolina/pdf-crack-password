3ef984e675a8a22442e5ad5deba0f6b1b9bc6aa3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-29 20:48:08 +0100

    Refactor use pahtlib

--- src/crack-pdf.py
+++ src/crack-pdf.py
@@ -1,22 +1,33 @@
 from tqdm import tqdm
+import pathlib
 import pikepdf
 import typing as tp
 
 
-class PathsConfig:
+class PathNamesConfig:
     @property
     def wordlist(self) -> str:
-        return "files/wordlist.txt"
+        file_name = "wordlist.txt"
+        return str(self._files_path.joinpath(file_name))
 
     @property
     def pdf(self) -> str:
-        return "files/secret-document.pdf"
+        file_name = "secret-document.pdf"
+        return str(self._files_path.joinpath(file_name))
+
+    @property
+    def _files_path(self) -> pathlib.Path:
+        return self._this_script_path.parent.joinpath("files")
+
+    @property
+    def _this_script_path(self) -> pathlib.Path:
+        return pathlib.Path(__file__).parent.absolute()
 
 
 class PdfCracker:
-    def __init__(self, passwords: tp.List[str], paths_config: PathsConfig):
+    def __init__(self, passwords: tp.List[str], path_names: PathNamesConfig):
         self._passwords = passwords
-        self._paths_config = paths_config
+        self._path_names = path_names
 
     def get_pdf_password(self) -> tp.Optional[str]:
         for password in tqdm(self._passwords, "Decrypting PDF"):
@@ -26,25 +37,26 @@ class PdfCracker:
 
     def _is_password_correct(self, password: str) -> bool:
         try:
-            with pikepdf.open(self._paths_config.pdf, password=password):
+            with pikepdf.open(self._path_names.pdf, password=password):
                 return True
         except pikepdf._qpdf.PasswordError:
             return False
 
 
 def run():
-    paths_config = PathsConfig()
-    passwords = get_passwords(paths_config)
-    password = PdfCracker(passwords, paths_config).get_pdf_password()
+    path_names = PathNamesConfig()
+    passwords = get_passwords(path_names)
+    password = PdfCracker(passwords, path_names).get_pdf_password()
     if password is None:
         print(f"\n[-] No password found")
     else:
         print(f"\n[+] Password found: {password}")
 
 
-def get_passwords(paths_config: PathsConfig) -> tp.List[str]:
-    return [line.strip() for line in open(paths_config.wordlist)]
+def get_passwords(path_names: PathNamesConfig) -> tp.List[str]:
+    return [line.strip() for line in open(path_names.wordlist)]
 
 
 if __name__ == "__main__":
     run()
+
