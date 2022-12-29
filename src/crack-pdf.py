de6b7f5bfd1c691218d127a5ea256a5ad61cfcac
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-29 20:31:17 +0100

    Refactor extract classes

--- src/crack-pdf.py
+++ src/crack-pdf.py
@@ -3,32 +3,47 @@ import pikepdf
 import typing as tp
 
 
+class PathsConfig:
+    @property
+    def wordlist(self) -> str:
+        return "files/wordlist.txt"
+
+    @property
+    def pdf(self) -> str:
+        return "files/secret-document.pdf"
+
+
+class PdfCracker:
+    def __init__(self, passwords: tp.List[str], paths_config: PathsConfig):
+        self._passwords = passwords
+        self._paths_config = paths_config
+
+    def get_pdf_password(self) -> tp.Optional[str]:
+        for password in tqdm(self._passwords, "Decrypting PDF"):
+            print(f"Checking password: {password}")
+            if self._is_password_correct(password):
+                return password
+
+    def _is_password_correct(self, password: str) -> bool:
+        try:
+            with pikepdf.open(self._paths_config.pdf, password=password):
+                return True
+        except pikepdf._qpdf.PasswordError:
+            return False
+
+
 def run():
-    passwords = get_passwords()
-    password = get_pdf_password(passwords)
+    paths_config = PathsConfig()
+    passwords = get_passwords(paths_config)
+    password = PdfCracker(passwords, paths_config).get_pdf_password()
     if password is None:
         print(f"\n[-] No password found")
     else:
         print(f"\n[+] Password found: {password}")
 
 
-def get_passwords() -> tp.List[str]:
-    return [line.strip() for line in open("files/wordlist.txt")]
-
-
-def get_pdf_password(passwords: tp.List[str]) -> tp.Optional[str]:
-    for password in tqdm(passwords, "Decrypting PDF"):
-        print(f"Checking password: {password}")
-        if is_password_correct(password):
-            return password
-
-
-def is_password_correct(password: str) -> bool:
-    try:
-        with pikepdf.open("files/secret-document.pdf", password=password) as pdf:
-            return True
-    except pikepdf._qpdf.PasswordError as e:
-        return False
+def get_passwords(paths_config: PathsConfig) -> tp.List[str]:
+    return [line.strip() for line in open(paths_config.wordlist)]
 
 
 if __name__ == "__main__":
